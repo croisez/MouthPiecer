cd72cfcec1c81b99180a75b3b88b7e9a35bc73dd
Louis-Marie Croisez <louis.croisez@gmail.com>
2023-10-19 10:25:55 +0200

    Reorganize sources to isolate Ethernet stuffs in its own ino file + remove code for breath control, as it is no longer a target for my project + introduce possible selection between MIDI DIN-5 or UsbMidi ports

---
+++ Ethernet.ino
@@ -0,0 +1,111 @@
+#include <SPI.h>
+#include <NativeEthernet.h>
+#include <TeensyThreads.h>
+
+#define USE_DHCP
+#define ETHERNET_INIT_TIMEOUT 7
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //Here choose MAC address for Ethernet module
+#ifndef USE_DHCP
+IPAddress ip(192, 168, 1, 50);
+#endif
+EthernetServer server(80);
+EthernetClient client;
+
+
+void EthernetBegin() {
+    threads.addThread(ethInit);
+  Serial.println("Searching for ethernet connection...");
+  long ethSavedTime = millis();
+  while(!connectedToEth && millis() - ethSavedTime < 1000 * ETHERNET_INIT_TIMEOUT) { delay(1000); }
+  if(connectedToEth){
+    Serial.println("Connected to ethernet");
+    Serial.print("IP address: ");
+    Serial.println(Ethernet.localIP());
+#ifdef USE_DHCP
+    EthernetRequestDhcpLease();
+#endif
+    server.begin();
+  }
+  else{
+    Serial.println("Running without ethernet");
+  }
+}
+
+void ethInit(){
+  while(!connectedToEth){
+    Ethernet.begin(mac 
+#ifndef USE_DHCP
+    , ip
+#endif
+    );
+    connectedToEth = true;
+  }
+  //Serial.println("End of ethInit thread");
+}
+
+void EthernetRequestDhcpLease() {
+  switch (Ethernet.maintain()) {
+  case 1: //renewed fail
+    Serial.println("Error: renewed fail");
+    return false;
+    break;
+
+  case 2: //renewed success
+    Serial.println("Renewed success.");
+    Serial.print("IP address: ");
+    Serial.println(Ethernet.localIP());
+    return true;
+    break;
+
+  case 3: //rebind fail
+    Serial.println("Error: rebind fail");
+    return false;
+    break;
+
+  case 4: //rebind success
+    Serial.println("Rebind success.");
+    Serial.print("IP address: ");
+    Serial.println(Ethernet.localIP());
+    return true;
+    break;
+
+  default: //nothing happened
+    return true;
+    break;
+  }
+}
+
+//Embryon of a web service, for exchanging information through a web browser with this application
+void listenForEthernetClients() {
+  EthernetClient client = server.available(); // listen for incoming clients
+  if (client) {
+    boolean currentLineIsBlank = true; // an http request ends with a blank line
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        // if you've gotten to the end of the line (received a newline character) and the line is blank, the http request has ended, so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+          int pbv = map( constrain(pitchBendAnalog, pitchbendMin, pitchbendMax), pitchbendMin, pitchbendMax, PB_MIN, PB_MAX);
+          // print the current readings, in HTML format:
+          client.print("AnalogPitch: [" + String(pitchbendMin) + " < " + String(pitchBendAnalog) + " < " + String(pitchbendMax) + "]");
+          client.println("<br />");
+          client.print("Pitch: " + String(pbv));
+          client.println("<br />");
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
--- MouthPiecer.ino
+++ MouthPiecer.ino
@@ -1,112 +1,87 @@
 // https://forum.pjrc.com/threads/66835-T4-Using-USB-Host-with-MIDI
-// https://www.pjrc.com/teensy/td_midi.html
 // https://little-scale.blogspot.com/2021/04/midi-usb-host-to-and-from-midi-5-pin.html
 // https://github.com/vjmuzik/NativeEthernet/tree/master/examples
+// https://www.electronicshub.org/hall-effect-sensor-with-arduino/
+// https://github.com/FortySevenEffects/arduino_midi_library
+// https://www.pjrc.com/teensy/td_libs_MIDI.html
+// https://www.pjrc.com/teensy/td_midi.html
 #include <USBHost_t36.h>
 #include <ADC.h>
 #include <ADC_util.h>
-#include <SPI.h>
-#include <NativeEthernet.h>
-#include <TeensyThreads.h>
+#include <MIDI.h>
 
 #define VERSION    "1"
-#define SUBVERSION "0-beta"
-#define USE_DHCP
-#define ETHERNET_INIT_TIMEOUT 7
-//#define ON_Thr 4 //comment calibrer cette valeur? Il faut que si on ne souffle pas, aucun son ne soit produit. Mais attention, il faut éviter un point dur au démarrage du souffle.
+#define SUBVERSION "1-beta"
+
+// Your configuration here =>
+// You can send MIDIOUT through DIN-5 plug, or through usbMidi device. Use USE_DIN_FOR_MIDIOUT define for this purpose.
+//#define USE_DIN_FOR_MIDIOUT
+// You can use a Teensy4.1 Ethernet adapter to have access to web service for configuration and debug. Use USE_ETHERNET define for this purpose.
+//#define USE_ETHERNET
+// You can generate fake MIDI traffic as a demo, when no MIDI device is connected on the USBHost MIDI port. Use USE_DEMO define for this purpose.
+//#define USE_DEMO
 #define PB_MIN -8192
 #define PB_MAX 8191
+// <= Your configuration here
 
-//int breathLevel;
-//int oldBreath=0;
-//int pressureMin = 1023;
-//int pressureMax = 0;
+#ifdef USE_DIN_FOR_MIDIOUT
+  MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+#endif
+
 int pitchbendMin = 5000;
 int pitchbendMax = -5000;
 u_int16_t pitchBendAnalog = 0;
 int oldPitchbendAnalog = 0;
 long lastPitchbendTime = 0;
-
 byte LedPin = 13;
-
 ADC *adc = new ADC();
 USBHost usbhost;
 USBHub hub1(usbhost);
 USBHub hub2(usbhost);
 MIDIDevice usbhostMIDI(usbhost);
-
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //Here choose MAC address for Ethernet module
-#ifndef USE_DHCP
-IPAddress ip(192, 168, 1, 50);
-#endif
-EthernetServer server(80);
-EthernetClient client;
+boolean connectedToEth = false;
 long lastEthernetClientTime = 0;
-volatile boolean connectedToEth = false;
 
-void ethInit(){
-  while(!connectedToEth){
-    Ethernet.begin(mac 
-#ifndef USE_DHCP
-    , ip
-#endif
-    );
-    connectedToEth = true;
-  }
-  //Serial.println("End of ethInit thread");
-}
 
 void debugPlot(String varname, int val, int next = 0){
-  Serial.print(varname); Serial.print(":"); 
-  Serial.print(val);
+  Serial.print(varname); Serial.print(":");  Serial.print(val);
   if (next) {Serial.print(", ");} else {Serial.print("\n");}
 }
 
 void SendNoteOn(byte note, byte velocity = 127, byte channel = 1) {
-  // midiSend((0x90 | (channel-1))), 0x50, 127);
+#ifdef USE_DIN_FOR_MIDIOUT
+  MIDI.sendNoteOn(note, velocity, channel);
+#else
   usbMIDI.sendNoteOn(note, velocity, channel);
+#endif
 }
 
 void SendNoteOff(byte note, byte channel = 1) {
-  // midiSend((0x80 | (channel-1))), 0x50, 127);
+#ifdef USE_DIN_FOR_MIDIOUT
+  MIDI.sendNoteOff(note, 0, channel);
+#else
   usbMIDI.sendNoteOff(note, 0, channel);
+#endif
 }
 
 void SendControlChange(byte cc, byte value, byte channel = 1) {
-  //midiSend((0xB0 | (channel-1)), cc, value);
+#ifdef USE_DIN_FOR_MIDIOUT
+  MIDI.sendControlChange(cc, value, channel);
+#else
   usbMIDI.sendControlChange(cc, value, channel);
+#endif
 }
 
 void SendPitchBend(int pitchBend, byte channel = 1) {
-  /*debugPlot("PB_MIN", PB_MIN, 1);
-  debugPlot("PB_MAX", PB_MAX, 1);
-  debugPlot("pitchBend", pitchBend);*/
+#ifdef USE_DIN_FOR_MIDIOUT
+  MIDI.sendPitchBend(pitchBend, channel);
+#else
   usbMIDI.sendPitchBend(pitchBend, channel);
+#endif
 }
 
-//  Send a three byte midi message to the MIDI DIN-5pin connector
-/*void midiSend(byte midistatus, byte data1, byte data2) 
-{
-  digitalWrite(LedPin,HIGH);  // indicate we're sending MIDI data
-  Serial1.write(midistatus);
-  Serial1.write(data1);
-  Serial1.write(data2);
-  digitalWrite(LedPin,LOW);  // indicate we're sending MIDI data
-}*/
 
-/*
-void CaptureBreath(){
-  //breathLevel = adc->analogRead(A0); adc->resetError();
-  //debugPlot("minP", pressureMin, 1); debugPlot("breathP", breathLevel, 1); debugPlot("maxP", pressureMax,1);
-
-  breathLevel = oldBreath*0.0+breathLevel*1.0;
-  oldBreath = breathLevel;
-  breathLevel = map(constrain(breathLevel, pressureMin + ON_Thr, pressureMax), pressureMin + ON_Thr, pressureMax, 0, 127);
-  SendControlChange(2, breathLevel);
-}
-*/
-
-// Le controleur USB_O2 envoie 32 messages maximum de 0 à 8191, et idem de -8192 à 0, en 128 ms. Ce qui fait 4 ms par message de PitchBend.
+// Exemple: le controleur USB_O2 envoie 32 messages maximum de 0 à 8191, et idem de -8192 à 0, en 128 ms. Ce qui fait 4 ms par message de PitchBend.
 void CapturePitchBend() {
   pitchBendAnalog = adc->analogRead(A1);
   adc->resetError();
@@ -119,8 +94,10 @@ void CapturePitchBend() {
     int pitchBendValue = map( constrain(pitchBendAnalog, pitchbendMin, pitchbendMax) , pitchbendMin, pitchbendMax , PB_MIN , PB_MAX);
     SendPitchBend(pitchBendValue);
     digitalWrite(LedPin, LOW);
+    
+    //debugPlot("PB_MIN", PB_MIN, 1); debugPlot("PB_MAX", PB_MAX, 1); debugPlot("pitchBend", pitchBend);
     //Serial.println(String(pitchbendMin) + "," + String(pitchBendAnalog) + "," + String(pitchbendMax));
-    //Serial.println("-8192," + String(pitchBendValue) + ",8192");
+    //Serial.println(PB_MIN + "," + String(pitchBendValue) + "," + PB_MAX);
   }
 
   oldPitchbendAnalog = pitchBendAnalog;
@@ -132,44 +109,28 @@ void setup() {
   Serial.println("####  MouthPiecer v" + String(VERSION) + "." + String(SUBVERSION) + "  ####");
 
   pinMode(LedPin,OUTPUT);
-  //pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 
-  //adc->adc0->setAveraging(16);                                    // set number of averages
-  //adc->adc0->setResolution(12);                                   // set bits of resolution
-  //adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::MED_SPEED); // change the conversion speed
-  //adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED);     // change the sampling speed
   adc->adc1->setAveraging(32);                                    // set number of averages
   adc->adc1->setResolution(16);                                   // set bits of resolution
   adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::MED_SPEED); // change the conversion speed
   adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED);     // change the sampling speed
 
-  delay(1500);// Wait at least 1.5 seconds before turning on USB Host.
+  delay(1500); // Wait at least 1.5 seconds before turning USBHost ON.
+
+#ifdef USE_DIN_FOR_MIDIOUT
+  MIDI.begin();
+#endif
 
   usbhost.begin();
   usbhostMIDI.setHandleNoteOn(onMidiHostNoteOn);
   usbhostMIDI.setHandleNoteOff(onMidiHostNoteOff);
   usbhostMIDI.setHandleControlChange(onMidiHostControlChange);
 
-  //calibrateSensors();
-
-  threads.addThread(ethInit);
-  Serial.println("Searching for ethernet connection...");
-  long ethSavedTime = millis();
-  while(!connectedToEth && millis() - ethSavedTime < 1000 * ETHERNET_INIT_TIMEOUT) { delay(1000); }
-  if(connectedToEth){
-    Serial.println("Connected to ethernet");
-    Serial.print("IP address: ");
-    Serial.println(Ethernet.localIP());
-#ifdef USE_DHCP
-    EthernetRequestDhcpLease();
+#ifdef USE_ETHERNET
+  EthernetBegin();
 #endif
-    server.begin();
-  }
-  else{
-    Serial.println("Running without ethernet");
-  }
-  
+
   digitalWrite(LedPin, HIGH); delay(200); digitalWrite(LedPin, LOW); delay(200); 
   digitalWrite(LedPin, HIGH); delay(200); digitalWrite(LedPin, LOW); delay(200); 
   digitalWrite(LedPin, HIGH); delay(200); digitalWrite(LedPin, LOW); delay(200); 
@@ -179,43 +140,49 @@ void setup() {
 
 // ########################################### L O O P ###########################################
 void loop() {
-  if (connectedToEth && (millis() - lastEthernetClientTime > 500)) { //500ms = do not give too much priority to web screen refresh
+#ifdef USE_ETHERNET
+  if (connectedToEth && (millis() - lastEthernetClientTime > 500)) { //500ms => do not give too much priority to web screen refresh
     listenForEthernetClients();
     lastEthernetClientTime = millis();
   }
-  
+#endif
+
   ProcessMidiLoop();
-#ifdef DEMO
+
+#ifdef USE_DEMO
   demo1();
 #endif
 }
 
 // ########################################### DEMOS ###########################################
+// Use the demos when you want to generate MIDI traffic without pluging a real MIDI device on the USBHost MIDI port
 void demo1() {
     //byte note = random(30,85);
     byte note = 60;
     SendNoteOn(note);
-    SendControlChange(2,100);
+    SendControlChange(2, 100);
     delay(200);
     SendNoteOff(note);
 }
-
 void demo2() {
     int note = random(30,85);
     SendNoteOn(note);
     for(int i=0;i<400;i++){
-      //CaptureBreath();
       ProcessMidiLoop();
       delay(10);
     }
     SendNoteOff(note);
 }
-/* **************************************************************************** */
+// ########################################### DEMOS ###########################################
 
 void ProcessMidiLoop() {
   usbhost.Task();
   usbhostMIDI.read();
+
+#ifndef USE_DIN_FOR_MIDIOUT
   usbMIDI.read();
+#endif
+
   if (millis() - lastPitchbendTime > 4) { //Do not send another pitch bend message sooner than 4ms.
     CapturePitchBend();
     lastPitchbendTime = millis();
@@ -223,111 +190,14 @@ void ProcessMidiLoop() {
 }
 
 void onMidiHostNoteOn(byte channel, byte note, byte velocity) {
-  // debugPrint("Note On, ch="); debugPrint(channel); debugPrint(", note="); debugPrint(note); debugPrint(", velocity="); debugPrintln(velocity);
   SendNoteOn(note, velocity, channel);  
 }
 void onMidiHostNoteOff(byte channel, byte note, byte velocity) {
-  // debugPrint("Note Off, ch="); debugPrint(channel); debugPrint(", note="); debugPrint(note); debugPrint(", velocity="); debugPrintln(velocity);
   SendNoteOff(note);
 }
 void onMidiHostControlChange(byte channel, byte control, byte value) {
-  // debugPrint("Control Change, ch="); debugPrint(channel); debugPrint(", control="); debugPrint(control); debugPrint(", value="); debugPrint(value);
   SendControlChange(control, value);
 }
 
-#if 0
-void calibrateSensors(){
-  Serial.println("Start of sensors calibration for 5s...");
- 
-  for (int i=0; i<50; i++){
-      digitalWrite(LedPin, HIGH);
-      /*
-      int valueP = adc->analogRead(A0);
-      if (pressureMin > valueP) pressureMin = valueP;
-      if (pressureMax < valueP) pressureMax = valueP;
-      delay(100);
-      */
-      int valueB = adc->analogRead(A1);
-      if (pitchbendMin > valueB) pitchbendMin = valueB;
-      if (pitchbendMax < valueB) pitchbendMax = valueB;
-      delay(100);
-      digitalWrite(LedPin, LOW);
-  }
- 
-  Serial.println("End of sensors calibration:");
-  //Serial.print("Min pressure: "); Serial.print(pressureMin); Serial.print(", Max pressure: "); Serial.print(pressureMax);
-  Serial.print("Min pitchBend: "); Serial.print(pitchbendMin); Serial.print(", Max pitchBend: "); Serial.println(pitchbendMax);
-  delay(3000);
-  //for (u_int16_t i = pitchbendMin - 5; i < pitchbendMax + 5; i++) { int pbv = map( constrain(i, pitchbendMin, pitchbendMax), pitchbendMin, pitchbendMax, PB_MIN, PB_MAX); Serial.print(i); Serial.print("; ");Serial.println(pbv);}
-}
-#endif
 
-void EthernetRequestDhcpLease() {
-  switch (Ethernet.maintain()) {
-  case 1:
-    //renewed fail
-    Serial.println("Error: renewed fail");
-    return false;
-    break;
 
-  case 2:
-    //renewed success
-    Serial.println("Renewed success.");
-    Serial.print("IP address: ");
-    Serial.println(Ethernet.localIP());
-    return true;
-    break;
-
-  case 3:
-    //rebind fail
-    Serial.println("Error: rebind fail");
-    return false;
-    break;
-
-  case 4:
-    //rebind success
-    Serial.println("Rebind success.");
-    Serial.print("IP address: ");
-    Serial.println(Ethernet.localIP());
-    return true;
-    break;
-
-  default: //nothing happened
-    return true;
-    break;
-  }
-}
-
-//Embryon of a web service, for exchanging information through a web browser with this application
-void listenForEthernetClients() {
-  EthernetClient client = server.available(); // listen for incoming clients
-  if (client) {
-    boolean currentLineIsBlank = true; // an http request ends with a blank line
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        // if you've gotten to the end of the line (received a newline character) and the line is blank, the http request has ended, so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println();
-          int pbv = map( constrain(pitchBendAnalog, pitchbendMin, pitchbendMax), pitchbendMin, pitchbendMax, PB_MIN, PB_MAX);
-          // print the current readings, in HTML format:
-          client.print("AnalogPitch: [" + String(pitchbendMin) + " < " + String(pitchBendAnalog) + " < " + String(pitchbendMax) + "]");
-          client.println("<br />");
-          client.print("Pitch: " + String(pbv));
-          client.println("<br />");
-          break;
-        }
-        if (c == '\n') {
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    delay(1);
-    client.stop();
-  }
-}

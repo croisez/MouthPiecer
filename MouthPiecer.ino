f50ff39af4f1c70f11069a274cc860577b164a43
Louis-Marie Croisez <louis.croisez@gmail.com>
2023-11-09 18:24:24 +0100

    Added FFT computation on pitch bend signal

--- MouthPiecer.ino
+++ MouthPiecer.ino
@@ -2,45 +2,41 @@
 #include <ADC.h>
 #include <ADC_util.h>
 #include <MIDI.h>
+#include <Fast4ier.h>
+#include <complex.h>
 
-#define VERSION    "1"
-#define SUBVERSION "4-alpha"
-
-// Your configuration here =>
-
-// You can send MIDIOUT through DIN-5 plug, or through usbMidi device. Use USE_DIN_FOR_MIDIOUT define for this purpose.
-//#define USE_DIN_FOR_MIDIOUT
-
-// You can use a Teensy4.1 Ethernet adapter to have access to web service for configuration and debug. Use USE_ETHERNET define for this purpose.
-//#define USE_ETHERNET
-
-// You can generate fake MIDI traffic as a demo, when no MIDI device is connected on the USBHost MIDI port. Use USE_DEMO define for this purpose.
-//#define USE_DEMO
-
-// Uncomment to print PitchBend values in serial terminal
-//#define DEBUG_PITCHBEND
-
-// Uncomment to print MIDI messages in serial terminal
-//#define DEBUG_MIDI_MSG
-
-// Uncomment to allow reset of PitchBend min & max values while button is pressed
-#define RESET_PB_MINMAX_WITH_BUTTONPRESS
-
+// Your configuration here =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#define DOUBLE                 //if you have alot of RAM, uncoment the following for extra FFT precision.
+//#define USE_DIN_FOR_MIDIOUT  // You can send MIDIOUT through DIN-5 plug, or through usbMidi device. Use USE_DIN_FOR_MIDIOUT define for this purpose.
+//#define USE_ETHERNET         // You can use a Teensy4.1 Ethernet adapter to have access to web service for configuration and debug. Use USE_ETHERNET define for this purpose.
+//#define USE_DEMO             // You can generate fake MIDI traffic as a demo, when no MIDI device is connected on the USBHost MIDI port. Use USE_DEMO define for this purpose.
+//#define DEBUG_PITCHBEND      // Uncomment to print PitchBend values in serial terminal
+//#define DEBUG_MIDI_MSG       // Uncomment to print MIDI messages in serial terminal
+#define DEBUG_FFT            // Uncomment to print FFT values in serial terminal
+#define RESET_PB_MINMAX_WITH_BUTTONPRESS  // Uncomment to allow reset of PitchBend min & max values while button is pressed
 #define PB_MIN -8192
 #define PB_MAX 8191
 #define BREATH_VALUE_MIN 19
 #define CC_TRAVELSAX2 2
 #define BUTTON_PIN 8
-// <= Your configuration here
+#define PB_ARRAY_SIZE 8
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<= Your configuration here
+
+#define VERSION    "1"
+#define SUBVERSION "4-alpha"
 
 #ifdef USE_DIN_FOR_MIDIOUT
   MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 #endif
 
-int pitchbendMin = 2000;
-int pitchbendMax = -2000;
-u_int16_t pitchBendAnalog = 0;
-int oldPitchbendAnalog = 0;
+int pbMin = 2000;
+int pbMax = -2000;
+int pbAnalog = 0;
+complex pbArray[PB_ARRAY_SIZE];
+int fftIdx = 0;
+long lastFftTime = 0;
+
+int oldpbAnalog = 0;
 long lastPitchbendTime = 0;
 byte LedPin = 13;
 ADC *adc = new ADC();
@@ -56,6 +52,11 @@ byte currentBreathValue;
 long lastButtonTime = 0;
 
 void debugPlot(String varname, int val, int next = 0){
+  Serial.print(varname); Serial.print(":");  Serial.print(val);
+  if (next) {Serial.print(", ");} else {Serial.print("\n");}
+}
+
+void debugPlotf(String varname, double val, int next = 0){
   Serial.print(varname); Serial.print(":");  Serial.print(val);
   if (next) {Serial.print(", ");} else {Serial.print("\n");}
 }
@@ -108,33 +109,6 @@ void SendPitchBend(int pitchBend, byte channel = 1) {
 #endif
 }
 
-
-// Exemple: le controleur USB_O2 envoie 32 messages maximum de 0 à 8191, et idem de -8192 à 0, en 128 ms. Ce qui fait 4 ms par message de PitchBend.
-void CapturePitchBend() {
-  pitchBendAnalog = adc->analogRead(A1);
-  adc->resetError();
-
-  if (pitchbendMin > pitchBendAnalog) pitchbendMin = pitchBendAnalog;
-  if (pitchbendMax < pitchBendAnalog) pitchbendMax = pitchBendAnalog;
-
-//  if (abs(pitchBendAnalog - oldPitchbendAnalog) > 0) {
-    digitalWrite(LedPin, HIGH);
-    int pitchBendValue = map( constrain(pitchBendAnalog, pitchbendMin, pitchbendMax) , pitchbendMin, pitchbendMax , PB_MAX , PB_MIN);
-    SendPitchBend(pitchBendValue);
-    digitalWrite(LedPin, LOW);
-    
-#ifdef DEBUG_PITCHBEND  
-    //debugPlot("PB_MIN", PB_MIN, 1); debugPlot("PB_MAX", PB_MAX, 1); debugPlot("pitchBend", pitchBend);
-    Serial.println(String(pitchbendMin) + "," + String(pitchBendAnalog) + "," + String(pitchbendMax));
-    //Serial.println(String(PB_MIN) + "," + String(pitchBendValueLP) + "," + String(PB_MAX));
-#endif
-//  }
-
-  oldPitchbendAnalog = pitchBendAnalog;
-}
-
-
-
 // ########################################### DEMOS>> ###########################################
 // Use the demos when you want to generate MIDI traffic without pluging a real MIDI device on the USBHost MIDI port
 void demo() {
@@ -179,6 +153,63 @@ void onMidiHostControlChange(byte channel, byte control, byte value) {
   SendControlChange(control, ccValue);
 }
 
+// Exemple: le controleur USB_O2 envoie 32 messages maximum de 0 à 8191, et idem de -8192 à 0, en 128 ms. Ce qui fait 4 ms par message de PitchBend.
+void CapturePitchBend() {
+  pbAnalog = adc->analogRead(A1);
+  adc->resetError();
+
+  if (pbMin > pbAnalog) pbMin = pbAnalog;
+  if (pbMax < pbAnalog) pbMax = pbAnalog;
+
+//  if (abs(pbAnalog - oldpbAnalog) > 0) {
+    digitalWrite(LedPin, HIGH);
+    int pbValue = map( constrain(pbAnalog, pbMin, pbMax) , pbMin, pbMax , PB_MAX , PB_MIN);
+    SendPitchBend(pbValue);
+    digitalWrite(LedPin, LOW);
+    
+#ifdef DEBUG_PITCHBEND  
+    //debugPlot("PB_MIN", PB_MIN, 1); debugPlot("PB_MAX", PB_MAX, 1); debugPlot("pitchBend", pitchBend);
+    Serial.println(String(pbMin) + "," + String(pbAnalog) + "," + String(pbMax));
+    //Serial.println(String(PB_MIN) + "," + String(pbValue) + "," + String(PB_MAX));
+#endif
+//  }
+
+  oldpbAnalog = pbAnalog;
+}
+
+void ProcessFFT() {
+  double v[PB_ARRAY_SIZE];
+  int pb = adc->analogRead(A1);
+  pbArray[fftIdx++] = pb - (pbMax - pbMin)/2 - pbMin;
+
+  if (fftIdx >= PB_ARRAY_SIZE) {
+    Fast4::FFT(pbArray, PB_ARRAY_SIZE);
+
+    //Compute all modulos for each freq bands
+    for (int i = PB_ARRAY_SIZE / 2; i < PB_ARRAY_SIZE; i++) {
+      v[i] = sqrt(pbArray[i].re() * pbArray[i].re() + pbArray[i].im() * pbArray[i].im());
+    }
+
+    //Compare energy in each band, and try to isolate a wining frequency
+    for (int i = PB_ARRAY_SIZE / 2; i < PB_ARRAY_SIZE; i++) {
+
+    }
+
+#ifdef DEBUG_FFT
+    debugPlot(String("Min"), 0, 1);
+#endif
+    for (int i = PB_ARRAY_SIZE / 2; i < PB_ARRAY_SIZE; i++) {
+#ifdef DEBUG_FFT
+      debugPlotf(String(i), v[i], 1);
+#endif
+    }
+#ifdef DEBUG_FFT
+    debugPlot(String("Max"), 15, 0);
+#endif
+    fftIdx = 0;
+  }
+}
+
 void ProcessMidiLoop() {
   usbhost.Task();
   usbhostMIDI.read();
@@ -187,7 +218,11 @@ void ProcessMidiLoop() {
   usbMIDI.read();
 #endif
 
-  pitchBendAnalog = adc->analogRead(A1);
+  if (millis() - lastFftTime > 1) {
+    ProcessFFT();
+    lastFftTime = millis();
+  }
+
   if (millis() - lastPitchbendTime > 4) { //Do not send another pitch bend message sooner than 4ms.
     CapturePitchBend();
     lastPitchbendTime = millis();
@@ -250,8 +285,8 @@ void loop() {
       breathValueMin = currentBreathValue + 2;
       Serial.println("Minimum breath set to value " + String(breathValueMin));
 #ifdef RESET_PB_MINMAX_WITH_BUTTONPRESS
-      pitchbendMin = 2000;
-      pitchbendMax = -2000;
+      pbMin = 2000;
+      pbMax = -2000;
 #endif
     }
     lastButtonTime = millis();
